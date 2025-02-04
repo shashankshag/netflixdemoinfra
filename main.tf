a600a912235f6b428a3e9fdf4ee4c9b63dcfd5a9
shashankshag <shashankindia592@gmail.com>
2025-02-04 07:25:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "swikp"
+key_name = "ps1"
 vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
 Name = var.instance_names[count.index]
